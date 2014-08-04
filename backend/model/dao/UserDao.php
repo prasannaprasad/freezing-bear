1e8312a1ab5c579f69dd8d403577c393110dff46
prasannaprasad <prasanna.prasad@yahoo.com>
2014-08-04 12:17:46 +0530

    User friends and User dao added

--- backend/model/dao/UserDao.php
+++ backend/model/dao/UserDao.php
@@ -14,10 +14,33 @@ class UserDao
         $query = "Select * from Usertest where fb_id='" . $user_id . "'";
         $result = $db->getSingleRecord($query);
 
-        $user = new User($result["id"],$result["fb_id"],$result["first_name"],$result["last_name"],$result["create_time"],
-                         $result["mod_time"],$result["fb_access_token"],$result["profile_pic"],$result["gender"],$result["hometown_name"],
-                         $result["relationship_status"],$result["birthdate"]);
+       // $user = new User($result["id"],$result["fb_id"],$result["first_name"],$result["last_name"],$result["create_time"],
+                //         $result["mod_time"],$result["fb_access_token"],$result["profile_pic"],$result["gender"],$result["hometown_name"],
+                  //       $result["relationship_status"],$result["birthdate"]);
 
-        return $user;
+      //  return $user;
+    }
+
+    public function addUser($fb_id,$first_name,$last_name,$create_time,$mod_time, $profile_pic,
+                         $gender,$hometown_name,$relationship_status,$birthdate,$email,$name,$location,$timezone)
+    {
+        $db = DBConnection::getInstance()->getHandle();
+
+        $prepared_query = " INSERT  into Usertest(fb_id,first_name,last_name,name,email,location,gender,relationship_status,
+                            timezone,create_time,birthdate,profile_pic) values (?,?,?,?,?,?,?,?,?,?,?,?)";
+        $create_time = date("Y-m-d H:i:s");
+
+        $stmt = $db->getPreparedStatement($prepared_query);
+        $stmt->bind_param("ssssssssssss",$fb_id,$first_name,$last_name,$name,$email,$location,$gender,$relationship_status,$timezone,
+                                         $create_time,$birthdate,$profile_pic);
+
+        if(!($status = $stmt->execute()))
+            throw new WebServiceException("Unable to execute query  " ,3017,__FILE__,__LINE__);
+        $id = $stmt->insert_id;
+        $stmt->close();
+
+        error_log("User $name inserted with $id and status $status");
+        return new User($id,$fb_id,$first_name,$last_name,$create_time,$profile_pic,$gender,$hometown_name,$relationship_status,
+                        $birthdate,$email,$name,$location,$timezone);
     }
 }
---
+++ backend/model/dao/UserFriendsDao.php
@@ -0,0 +1,30 @@
+<?php
+include_once('DBConnection.php');
+include_once __SITE_PATH . '/model/entities/' . 'UserFriends.php';
+
+class UserFriendsDao
+{
+
+    public function addFriend($source_friend_id,$target_friend_id)
+    {
+        $db = DBConnection::getInstance()->getHandle();
+
+        $prepared_query = " INSERT IGNORE into UserFriends(source_friend_id,target_friend_id,create_time)   values (?,?,?)";
+        $create_time = date("Y-m-d H:i:s");
+
+        $stmt = $db->getPreparedStatement($prepared_query);
+        $stmt->bind_param("sss",$source_friend_id,$target_friend_id,$create_time);
+
+        if(!($status = $stmt->execute()))
+            throw new WebServiceException("Unable to execute query  " ,3017,__FILE__,__LINE__);
+        $stmt->bind_param("sss",$target_friend_id,$source_friend_id,$create_time);
+
+        if(!($status = $stmt->execute()))
+            throw new WebServiceException("Unable to execute query  " ,3017,__FILE__,__LINE__);
+        $id = $stmt->insert_id;
+        $stmt->close();
+
+        error_log("Two way Friend link created between $source_friend_id and $target_friend_id ");
+        return new UserFriend($id,$source_friend_id,$target_friend_id,$create_time);
+    }
+}
